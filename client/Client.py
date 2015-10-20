4977d88a0dd39028b69fcabb76460b68334e6819
Lisen <lisenschakonat@gmail.com>
2015-10-20 12:29:42 +0200

    client
    de klientdelarna jag jobbat på (och trott att jag har laddat upp på
    gittan tidigare fail!!!)

---
+++ client/Client.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from bottle import *
+from testbackend import skicka
+HOST = "localhost"
+
+@route("/static/<filepath:path>")
+def server_static(filepath):
+    """CSS"""
+    return static_file(filepath, root="static")
+   
+@route('/')
+def start(): 
+    """kÃ¶r bara index templatet
+    """
+    return template('index')
+
+@route('/playlist', method="POST")
+def get_request():
+    """hÃ¤mtar in svaret som anvÃ¤ndaren skrivier in i playlist och retunar ett 
+    ett tempalte med en rubrik som Ã¤r playlisten
+    """
+    req = request.forms.req
+    return template('playlist', req = skicka(req))
+
+'''
+@error(404)
+def error404(error):
+    """felmeddelande för 404"""
+    fel = "lägg av med det där! försök inte surfa in på massa grejer som inte finns! använd länkarna istället :)  "
+    return template('felmeddelande', fel = fel)
+'''
+run(host = HOST, port = 8080, debug=True,)
---
+++ client/testbackend.py
@@ -0,0 +1,6 @@
+# -- coding: utf-8 --
+
+
+def skicka(req):
+    sant = str(req) + 'knas'
+    return sant
